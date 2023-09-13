612a6c9d56bbeb3ce35fbe9b00dc56bdfa51db8f
mk812y <mk812y@gmail.com>
2023-09-13 21:00:02 +0200

    ok

--- Presenting Content/ContentView.swift
+++ Presenting Content/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columnLayout = Array(repeating: GridItem(), count: 3)
+    @State private var selectedColor = Color.gray
+    
+    let allColors: [Color] = [
+        .pink,
+        .red,
+        .orange,
+        .yellow,
+        .green,
+        .mint,
+        .teal,
+        .cyan,
+        .blue,
+        .indigo,
+        .purple,
+        .brown,
+        .gray,
+        .black
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Selected Color")
+                .font(.body)
+                .fontWeight(.semibold)
+                .foregroundColor(selectedColor)
+                .padding(10)
         }
         .padding()
+        
+        ScrollView {
+            LazyVGrid(columns: columnLayout) {
+                ForEach(allColors, id: \.description) { color in
+                    Button {
+                        selectedColor = color
+                    } label: {
+                        RoundedRectangle(cornerRadius: 4.0)
+                            .aspectRatio(1.0, contentMode: ContentMode.fit)
+                            .foregroundColor(color)
+                    }
+//                    .buttonStyle(.plain)
+                }
+            }
+        }
+        .padding(10)
     }
 }
 
