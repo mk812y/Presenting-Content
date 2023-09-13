e07953a9d803da19d7f5a66091746bac12eaa8a2
mk812y <mk812y@gmail.com>
2023-09-13 20:26:17 +0200

    Initial Commit

---
+++ Presenting Content/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Presenting Content
+//
+//  Created by Михаил Куприянов on 13.9.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Presenting Content/Presenting_ContentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Presenting_ContentApp.swift
+//  Presenting Content
+//
+//  Created by Михаил Куприянов on 13.9.23..
+//
+
+import SwiftUI
+
+@main
+struct Presenting_ContentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
